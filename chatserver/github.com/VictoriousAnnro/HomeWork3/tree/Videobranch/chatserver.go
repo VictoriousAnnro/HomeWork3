6fcf11f2a784153e30802819a71a248b7301cbdc
eikbo <eikb@itu.dk>
2022-10-24 22:20:54 +0200

    Step One

---
+++ chatserver/github.com/VictoriousAnnro/HomeWork3/tree/Videobranch/chatserver.go
@@ -0,0 +1,104 @@
+package Videobranch
+
+import (
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type messageUnit struct {
+	ClientName        string
+	MessageBody       string
+	MessageUniqueCode int
+	ClientUniqueCode  int
+}
+
+type messageHandle struct {
+	MQue []messageUnit
+	mu   sync.Mutex
+}
+
+var messageHandleObject = messageHandle{}
+
+type ChatServer struct {
+}
+
+func (is *ChatServer) ChatService(csi Services_ChatServiceServer) error {
+
+	clientUniqueCode := rand.Intn(1e6)
+	errch := make(chan error)
+
+	go recieveFromStream(csi, clientUniqueCode, errch)
+	go sendToStream(csi, clientUniqueCode, errch)
+
+	return <-errch
+
+}
+
+func recieveFromStream(csi_ Services_ChatServiceServer, clientUniqueCode_ int, errch_ chan error) {
+
+	for {
+		mssg, err := csi_.Recv()
+		if err != nil {
+			log.Printf("Error in reciving message from client :: %v", err)
+			errch_ <- err
+		} else {
+
+			messageHandleObject.mu.Lock()
+
+			messageHandleObject.MQue = append(messageHandleObject.MQue, messageUnit{
+				ClientName:        mssg.Name,
+				MessageBody:       mssg.Body,
+				MessageUniqueCode: rand.Intn(1e8),
+				ClientUniqueCode:  clientUniqueCode_,
+			})
+
+			messageHandleObject.mu.Unlock()
+			log.Printf("%v", messageHandleObject.MQue[len(messageHandleObject.MQue)-1])
+		}
+
+	}
+
+}
+
+func sendToStream(csi_ Services_ChatServiceServer, clientUniqueCode_ int, errch_ chan error) {
+	for {
+		time.Sleep(500 * time.Millisecond)
+
+		messageHandleObject.mu.Lock()
+
+		if len(messageHandleObject.MQue) == 0 {
+			messageHandleObject.mu.Unlock()
+			break
+		}
+
+		senderUniqueCode := messageHandleObject.MQue[0].ClientUniqueCode
+		senderName4Client := messageHandleObject.MQue[0].ClientName
+		message4Client := messageHandleObject.MQue[0].MessageBody
+
+		messageHandleObject.mu.Unlock()
+
+		if senderUniqueCode != clientUniqueCode_ {
+			err := csi_.Send(&FromServer{Name: senderName4Client, Body: message4Client})
+
+			if err != nil {
+				errch_ <- err
+			}
+			messageHandleObject.mu.Lock()
+
+			if len(messageHandleObject.MQue) > 1 {
+				messageHandleObject.MQue = messageHandleObject.MQue[1:]
+			} else {
+				messageHandleObject.MQue = []messageUnit{}
+
+			}
+
+			messageHandleObject.mu.Unlock()
+		}
+
+		time.Sleep(100 * time.Millisecond)
+
+	}
+
+}
