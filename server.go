cfc59086cc80185c1d5093e4b282265c2af4706a
eikbo <eikb@itu.dk>
2022-10-27 11:18:07 +0200

    wow

--- server.go
+++ server.go
@@ -12,6 +12,8 @@ import (
 
 func main() {
 
+	f := setLog()
+	defer f.Close()
 	Port := os.Getenv("PORT")
 	if Port == "" {
 		Port = "5000"
@@ -33,4 +35,19 @@ func main() {
 		log.Fatalf("Failed to start gRPC server :: %v", err)
 	}
 
+}
+
+func setLog() *os.File {
+	// Clears the log.txt file when a new server is started
+	if err := os.Truncate("log.txt", 0); err != nil {
+		log.Printf("Failed to truncate: %v", err)
+	}
+
+	// This connects to the log file/changes the output of the log informaiton to the log.txt file.
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	return f
 }
